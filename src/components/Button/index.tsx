54c9ccf5e1ea2516d4116e4a1e245b16c57ec116
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-12-28 17:17:20 +0530

    add eslint for storybook

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,13 @@
+import {ComponentProps} from "react";
+
+
+type ButtonProps=ComponentProps<"button">;
 
 
 const Button
- = () => {
+ = ({...props}:ButtonProps) => {
   return (
-    <button className="text-blue-500">Button
+    <button className="text-red-500" {...props}>
 
     </button>
   )
