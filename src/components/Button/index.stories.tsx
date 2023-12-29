acfba767bd5280a232477a8b2ef0c4588ef90672
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-12-29 21:35:28 +0530

    added dependencies and styles for button

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -16,21 +16,22 @@ type Story = StoryObj<typeof meta>;
 
 export const Solid: Story = {
   args: {
-    // variant: 'solid',
-    children: 'click me ',
+    variant: "solid",
+   children:"Button",
+   
   },
 };
 
-// export const Outline: Story = {
-//   args: {
-//     variant: 'outline',
-//     children: 'Button',
-//   },
-// };
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+    children: 'Button',
+  },
+};
 
-// export const Ghost: Story = {
-//   args: {
-//     variant: 'ghost',
-//     children: 'Button',
-//   },
-// };
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+    children: 'Button',
+  },
+};
